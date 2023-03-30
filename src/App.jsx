86ffd1f4def8b85ad256071480d555ba002226ad
Kévin Vallin <kevin.vallin@ynov.com>
2023-03-30 12:31:00 +0200

    Add Bluetooth

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
+    const [device, setDevice] = useState(null);
+    const [info, setInfo] = useState('');
+
+    const connectToDevice = async () => {
+      try {
+        const device = await navigator.bluetooth.requestDevice({
+          filters: [{ services: ['battery_service'] }],
+        });
+
+        setDevice(device);
+        setInfo(`Connected to ${device.name}`);
+      } catch (error) {
+        setInfo(`Error: ${error.message}`);
+      }
+    };
+  
 
   return (
     <>
       <div className="App">
+        <div className="App">
+          <button onClick={connectToDevice}>Connect to Device</button>
+          <p>{info}</p>
+          <p>{device}</p>
+        </div>
+        <br />
         <div className="App">
           <button>Allumez LED 1</button>
         </div>
